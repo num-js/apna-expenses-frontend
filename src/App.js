5f804d1970945c38abb2ba82f58387ac8fc9b5cf
Numan Ahmed <mdnmnahmed@gmail.com>
2021-03-28 12:32:29 +0530

    Working on Expenses cards components

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import Card from './components/Card';
+import './styles/app.css';
+import AddExpenses from './components/Modals/AddExpensesModal';
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
+
+    const [allExpenses, setAllExpenses] = useState([]);
 
     useEffect(() => {
         // Init Materialize JS
         M.AutoInit();
     });
+
 
     return (
         <>
@@ -31,6 +38,19 @@ function App() {
                         pauseOnHover
                     />
                     <div className="container">
+
+                        {/* Add Expenses Button */}
+                        <div class="fixed-action-btn">
+                            <a class="btn-floating btn-large z-depth-5 pulse darken-3">
+                                <i class="large material-icons modal-trigger" href="#modal1">add</i>
+                            </a>
+                        </div>
+
+                        {/* Add Expenses Modal */}
+                        <AddExpenses
+                            setAllExpenses={setAllExpenses}
+                        />
+
                         <div className="">
                             <br />
                             <div class="row">
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,9 @@ const Card = () => {
         <>
             <div class="col s12 m6 l4">
                 <div class="card sticky-action">
-                    <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator"  src={process.env.PUBLIC_URL + '/images/nlogo.jpg'} />
+                    <div style={{ height: "300px" }} class="card-image waves-effect waves-block waves-light responsive">
+                        <img class="activator responsive-img" src={process.env.PUBLIC_URL + '/images/nlogo.jpg'} />
+                        NNNNNN<br />
                     </div>
                     <div class="card-action">
                         <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
