bf3f555c375cff0c13603350d98c4a9d05c2f624
Numan Ahmed <mdnmnahmed@gmail.com>
2021-08-21 20:01:05 +0530

    removed old codes

--- src/App.js
+++ src/App.js
@@ -1,78 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import NavBar from './components/elements/NavBar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js';
-import Card from './components/Card';
-import './styles/app.css';
-import AddExpenses from './components/Modals/AddExpensesModal';
+import React from 'react';
 
-const API_URL = process.env.REACT_APP_API_URL;
-
-function App() {
-
-    const [allExpenses, setAllExpenses] = useState([]);
-
-    useEffect(() => {
-        // Init Materialize JS
-        M.AutoInit();
-    });
-
-
+const App = () => {
     return (
         <>
-            <Router>
-                <div className="App">
-                    <NavBar />
-                    <ToastContainer
-                        position="top-right"
-                        autoClose={5000}
-                        hideProgressBar={false}
-                        newestOnTop={false}
-                        closeOnClick
-                        rtl={false}
-                        pauseOnFocusLoss
-                        draggable
-                        pauseOnHover
-                    />
-                    <div className="container">
-
-                        {/* Add Expenses Button */}
-                        <div class="fixed-action-btn">
-                            <a class="btn-floating btn-large z-depth-5 pulse darken-3">
-                                <i class="large material-icons modal-trigger" href="#modal1">add</i>
-                            </a>
-                        </div>
-
-                        {/* Add Expenses Modal */}
-                        <AddExpenses
-                            setAllExpenses={setAllExpenses}
-                        />
-
-                        <div className="">
-                            <br />
-                            <div class="row">
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Router>
+            Hello Numan
         </>
     );
 }
