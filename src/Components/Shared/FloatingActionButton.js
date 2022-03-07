d5c1f263f0494bf4f6e03f319ddcf504da47a9fe
Numan Ahmed <mdnmnahmed@gmail.com>
2022-03-07 14:55:50 +0530

    Floating Action Button more configurable

--- src/Components/Shared/FloatingActionButton.js
+++ src/Components/Shared/FloatingActionButton.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 
 import Fab from "@mui/material/Fab";
 
-const FloatingActionButton = ({ children }) => {
+const FloatingActionButton = ({ children, positions = { bottom: 100, right: 30 }, color = 'primary' }) => {
 
     return (
         <>
             <Fab
-                sx={{ position: "fixed", bottom: 100, right: 30 }}
-                color="primary"
+                sx={{ position: "fixed", ...positions }}
+                color={color}
             // variant="extended"
             >
                 {children}
