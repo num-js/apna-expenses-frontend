ff0f78afa16e6aa1b5f02d51684a4152f75b37b0
Numan Ahmed <mdnmnahmed@gmail.com>
2022-03-16 15:05:20 +0530

    Khata UI as Chat UI

--- src/Components/Khata/KhataPage.js
+++ src/Components/Khata/KhataPage.js
@@ -31,27 +31,37 @@ const KhataPage = () => {
                         </FloatingActionButton>
                     </div>
 
-                    <section class="text-gray-600 body-font" style={{ minHeight: '90vh' }}>
-                        <div class="container px-5 py-4 mx-auto">
-                            <div class="flex flex-wrap -m-2">
-                                {allKhataTransactions && allKhataTransactions.map((transaction) => (
-                                    <div key={transaction._id} class="p-2 lg:w-1/3 md:w-1/2 w-full">
-                                        <div class="h-full flex items-center justify-between border-gray-200 border p-4 rounded-lg">
-                                            <div class="">
-                                                <h2 class="text-white title-font font-medium"> Khata {transaction.title} </h2>
-                                                <p class="text-gray-500"> {transaction.date} &nbsp; </p>
-                                            </div>
-                                            <div class="">
-                                                <h2 class="text-white title-font font-medium"> ₹ {transaction.amount} </h2>
-                                                <button className="relative" style={{ top: '34px', left: '41px', }}
-                                                // onClick={() => dispatch(deleteMoney(transaction._id))}
-                                                >
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#ee3e3e" viewBox="0 0 30 30" width="30px" height="30px">    <path d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z" /></svg>
-                                                </button>
+                    <section class="mt-5">
+                        <div class=" mx-auto">
+                            <div class="mx-1  rounded">
+                                <div>
+                                    <div class="w-full">
+                                        <div>
+                                            <div class="relative flex items-center p-3 border-b border-gray-300">
+                                                <img class="object-cover w-10 h-10 rounded-full" src="../images/nlogo.jpg" alt="username" />
+                                                <span class="block ml-2 font-bold text-gray-600">Mama</span>
+                                                <span class="absolute w-3 h-3 bg-green-600 rounded-full left-10 top-3"></span>
+                                                <span class="block ml-auto font-bold text-gray-600"> ₹ 150000</span>
                                             </div>
                                         </div>
+
+                                        <div class="relative w-full p-6 overflow-y-auto h-[40rem]">
+                                            <ul class="space-y-2">
+                                                {allKhataTransactions && allKhataTransactions.map((transaction) => (
+                                                    <li class={`flex ${transaction.transactionType === "receive" ? "justify-start" : "justify-end"}`}>
+                                                        <div class={`relative max-w-xl px-4 py-2 rounded shadow ${transaction.transactionType === "receive" ? 'text-white bg-blue-500' : 'text-gray-700 bg-gray-100'}`} style={{ minWidth: '60%' }}>
+                                                            <span class="block"> {transaction.title} </span>
+                                                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                                <div className="text-gray-200"> {transaction.date} </div>
+                                                                <div className="font-bold"> ₹ {transaction.amount} </div>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </div>
                                     </div>
-                                ))}
+                                </div>
                             </div>
                         </div>
                     </section>
